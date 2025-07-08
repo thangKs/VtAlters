422f14f3f020d7dbf751c04ad3e5198bde5eb45a
thangks <thangfc3512@gmail.com>
2025-07-08 17:10:48 +0700

    update license

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# VtAlters
+## License
+
+This project is licensed under the [MIT License](./LICENSE) © 2025 thangks.
--- src/main/java/com/vtalters/Altar.java
+++ src/main/java/com/vtalters/Altar.java
@@ -1,22 +1,13 @@
-package com.vtalters;
-
 /*
  * VtAlters - A plugin for summoning bosses via altars.
- * Copyright (C) 2025 thangks
+ * Copyright (c) 2025 thangks
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ * Licensed under the MIT License.
+ * See the LICENSE file in the root of this project for more information.
  */
+
+package com.vtalters;
+
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
